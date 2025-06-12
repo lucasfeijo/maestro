3358cc5617fa427f9ed0fb390f72ddbf9e6e6c05
Lucas Feijo <lucas.feijo@gmail.com>
2025-06-12 01:08:33 -0300

    merge diff structs

--- Sources/LightStateDiff.swift
+++ Sources/LightStateDiff.swift
@@ -8,10 +8,29 @@ public struct LightStateDiff {
         self.changes = changes
         self.currentStates = currentStates
     }
-}
 
-extension LightStateDiff {
-    public var simplified: LightStateDiffSimplified {
-        LightStateDiffSimplified(self)
+    /// A simplified list of states omitting changes that would not alter
+    /// the current Home Assistant state.
+    public var simplified: [LightState] {
+        var simplified: [LightState] = []
+        for change in changes {
+            if let current = currentStates[change.entityId],
+               let state = current["state"] as? String {
+                var shouldSend = false
+                if (state == "on") != change.on {
+                    shouldSend = true
+                } else if let desired = change.brightness,
+                          let attrs = current["attributes"] as? [String: Any],
+                          let curr = attrs["brightness"] as? Int {
+                    let pct = Int(round(Double(curr) * 100.0 / 255.0))
+                    if pct != desired { shouldSend = true }
+                } else if change.brightness != nil {
+                    shouldSend = true
+                }
+                if !shouldSend { continue }
+            }
+            simplified.append(change)
+        }
+        return simplified
     }
 }
